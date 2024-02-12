dc973052d4e3a011ca14593b0d9f8a00d8778026
HadiScript <2019cs519@gmail.com>
2024-02-12 17:43:42 +0500

    asdasd

--- src/helper/API.js
+++ src/helper/API.js
@@ -1,5 +1,5 @@
 // export const API = "https://quizer.cycarts.com";
-export const API = "https://quizer-backendv2.vercel.app/";
+export const API = "https://quizer-backendv2-psi.vercel.app";
 export const authApi = `${API}/api/auth`;
 export const quizApi = `${API}/api/quiz`;
 export const userApi = `${API}/api/user`;
