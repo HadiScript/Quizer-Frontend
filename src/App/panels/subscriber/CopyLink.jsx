83fe24e9343ccd57ebbfbf05290b88ad39ff2e54
HadiScript <2019cs519@gmail.com>
2024-03-08 12:40:35 +0500

    some changes

--- src/App/panels/subscriber/CopyLink.jsx
+++ src/App/panels/subscriber/CopyLink.jsx
@@ -1,12 +1,11 @@
 import { Button, Input, Modal } from "antd";
-import React from "react";
 import Heading from "../../components/common/Heading";
 import { CopyOutlined } from "@ant-design/icons";
 import toast from "react-hot-toast";
 
 const CopyLinkModal = ({ open, setOpen, quizId, creatorId }) => {
-  const link = `http://localhost:5173/attempt-quiz/${creatorId}/${quizId}`;
-  // const link = `https://quizer-frontend.vercel.app//attempt-quiz/${creatorId}/${quizId}`;
+  // const link = `http://localhost:5173/attempt-quiz/${creatorId}/${quizId}`;
+  const link = `https://quizer-frontend.vercel.app//attempt-quiz/${creatorId}/${quizId}`;
 
   const copyToClipboard = (text) => {
     navigator.clipboard
