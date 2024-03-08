67a22b349e89b9b61ac403135c6dcc4e731aee83
HadiScript <2019cs519@gmail.com>
2024-03-08 13:22:09 +0500

    helo

--- src/App/panels/subscriber/CopyLink.jsx
+++ src/App/panels/subscriber/CopyLink.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 
 const CopyLinkModal = ({ open, setOpen, quizId, creatorId }) => {
   // const link = `http://localhost:5173/attempt-quiz/${creatorId}/${quizId}`;
-  const link = `https://quizer-frontend.vercel.app//attempt-quiz/${creatorId}/${quizId}`;
+  const link = `https://quizer-frontend.vercel.app/attempt-quiz/${creatorId}/${quizId}`;
 
   const copyToClipboard = (text) => {
     navigator.clipboard
