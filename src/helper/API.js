1b46b05b3025fbe6e4fc3d8c0bf64f5fa3e74d3b
HadiScript <2019cs519@gmail.com>
2024-03-08 13:23:57 +0500

    api updates

--- src/helper/API.js
+++ src/helper/API.js
@@ -1,7 +1,7 @@
 // export const API = "https://quizer.cycarts.com";
 
 // export const API = "http://localhost:8080";
-export const API = "https://quizer-backendv2.vercel.app/";
+export const API = "https://quizer-backendv2.vercel.app";
 export const authApi = `${API}/api/auth`;
 export const quizApi = `${API}/api/quiz`;
 export const userApi = `${API}/api/user`;
