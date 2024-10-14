8b25968f185fd1200b1173f3e5d3ffcbc55b1c07
hadi script <hadistructures@gmail.com>
2024-10-14 22:55:10 +0500

    asd

--- src/helper/API.js
+++ src/helper/API.js
@@ -2,6 +2,8 @@ export const API = "https://api.sawal.co";
 // export const API = "http://localhost:8080";
 export const authApi = `${API}/api/auth`;
 export const quizApi = `${API}/api/quiz`;
+
+
 export const userApi = `${API}/api/user`;
 export const questionApi = `${API}/api/question`;
 export const attemptApi = `${API}/api/attempt`;
